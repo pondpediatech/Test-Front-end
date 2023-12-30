4beafb0e1dbcb15d75dd8f18b7ac9230141b9a16
capsaicinoids <frizaaditya09@gmail.com>
2023-12-30 21:53:40 +0700

    Change Sidebar Logo

--- components/SidebarDashboard/index.tsx
+++ components/SidebarDashboard/index.tsx
@@ -66,9 +66,9 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
       <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
         <Link href="/">
           <Image
-            width={176}
+            width={126}
             height={32}
-            src={"/images/logo/logo.svg"}
+            src={"/images/logo/logo-2.png"}
             alt="Logo"
           />
         </Link>
