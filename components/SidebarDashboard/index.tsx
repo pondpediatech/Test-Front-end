979c9f20e3eaf886f9797b22c5add62edb3ee2fc
capsaicinoids <frizaaditya09@gmail.com>
2023-12-31 10:11:33 +0700

    Fix Some Lint Issues

--- components/SidebarDashboard/index.tsx
+++ components/SidebarDashboard/index.tsx
@@ -109,7 +109,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
               {/* <!-- Menu Item Dashboard --> */}
               <SidebarLinkGroup
                 activeCondition={
-                  pathname === "/account" || pathname?.includes("dashboard")
+                  pathname === "/account" || pathname!.includes("dashboard")
                 }
               >
                 {(handleClick, open) => {
@@ -204,7 +204,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                 <Link
                   href="/account/settings"
                   className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
-                    pathname.includes("settings") &&
+                    pathname!.includes("settings") &&
                     "bg-graydark dark:bg-meta-4"
                   }`}
                 >
@@ -258,7 +258,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                   }`}
                 >
                   <svg
-                    class="fill-current"
+                    className="fill-current"
                     width="18"
                     height="19"
                     viewBox="0 0 18 19"
